40caf424eebe2c727626a6114f0cfaf692b1361c
vijay <vijay@gmail.com>
2023-12-15 01:41:29 +0530

    Done

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"a2bebe2d-d930-42f9-bb10-2ed694b169d3","feature":"to test the Books Wagon page on chrome browser","scenario":"to test the Login page","start":1702584530319,"group":1,"content":"","tags":"@login,","end":1702584546875,"className":"passed"},{"id":"9625f401-19c8-4469-b884-ec4e215814dd","feature":"to test the Books Wagon page on chrome browser","scenario":"to test the Login page","start":1702584546937,"group":1,"content":"","tags":"@login,","end":1702584561870,"className":"failed"},{"id":"9939ce82-52d7-4852-8933-cf77c90136be","feature":"to test the Books Wagon page on chrome browser","scenario":"to test the Login page","start":1702584491216,"group":1,"content":"","tags":"@login,","end":1702584529613,"className":"failed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
